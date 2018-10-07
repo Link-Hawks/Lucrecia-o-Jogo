24852a2db5aa7036b42a57fd7372354534fae486
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2018-10-06 23:40:13 -0300

    atualização dos comentarios e criação do ajudante(inutilmente por enquanto)

--- Game/js/helpers/ColisaoHelper.js
+++ Game/js/helpers/ColisaoHelper.js
@@ -1,36 +1,33 @@
 class ColisaoHelper{
-    checarDano(Personagem,Inimigo){
-        if(golpeou){
-            let inimigoDireita = Inimigo.x > this.x && Inimigo.x<this.x+this.largura;
-            let inimigoEsquerda = Inimigo.x+Inimigo.largura>this.x && Inimigo.x<this.x;
-            console.log(inimigoEsquerda)
+    checarDano(Inimigo){
+        let inimigoDireita = Inimigo.x > this.x && Inimigo.x<this.x+this.largura;
+        let inimigoEsquerda = Inimigo.x+Inimigo.largura>this.x && Inimigo.x<this.x;
+        if(inimigoDireita && (Inimigo.golpeEspecial1 || Inimigo.golpeSoco)){
+            this.hp-=5;
+            if(this.checaColisao(Inimigo,"direita"))
+                this.x = Inimigo.x-Inimigo.largura-4;           
+        }else if(inimigoEsquerda && (Inimigo.golpeEspecial1 || Inimigo.golpeSoco)){
+            this.hp-=5; 
+            if(this.checaColisao(Inimigo,"esquerda")){
+                this.x = Inimigo.x+Inimigo.largura+1; 
+                this.xTemp = this.x;
+                console.log(this.nome+" || ")
+            }
+        }
+        if(inimigoDireita || inimigoEsquerda){
+            
+            var audio = document.createElement("audio");
+            audio.setAttribute("src","punch.mp3");
+            audio.play();
+        }
 
-            if(inimigoDireita && (Inimigo.golpeEspecial1 || Inimigo.golpeSoco)){
-                this.hp-=5;
-                if(this.checaColisao(Inimigo,"direita"))
-                    this.x = Inimigo.x-Inimigo.largura-4;           
-            }else if(inimigoEsquerda && (Inimigo.golpeEspecial1 || Inimigo.golpeSoco)){
-                this.hp-=5; 
-                if(this.checaColisao(Inimigo,"esquerda")){
-                    this.x = Inimigo.x+Inimigo.largura+1; 
-                    this.xTemp = this.x;
-                }
-            }
-            if(inimigoDireita || inimigoEsquerda){
-                
-                var audio = document.createElement("audio");
-                audio.setAttribute("src","punch.mp3");
-                audio.play();
-            }
-
-            //Temporario
-            if(this.hp<=0){
-                this.hp = 0;
-            }
-            golpeou = false;
+        //Temporario
+        if(this.hp<=0){
+            this.hp = 0;
         }
     }
 
-    checaAcertou(Per)
-
+    checarGolpeAcertou(Personagem,Inimigo){
+        
+    }
 }
